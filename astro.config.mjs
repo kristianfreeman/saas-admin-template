5d7e2b534ba35c42b01521a65d43ae12d716911f
Kristian Freeman <kristian@kristianfreeman.com>
2024-12-23 11:36:52 -0600

    Configure astro, add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  integrations: [react(), tailwind({ applyBaseStyles: true })]
 });
