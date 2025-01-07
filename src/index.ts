677598d589bde68f24b3de63e5114babe8106775
Kristian Freeman <kristian@kristianfreeman.com>
2025-01-07 11:57:11 -0600

    Update workflows usage

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { WorkflowEntrypoint, WorkflowStep } from 'cloudflare:workers';
+import type { WorkflowEvent } from 'cloudflare:workers';
+
+type Env = {
+  CUSTOMER_WORKFLOW: WorkflowEntrypoint<Env, Params>;
+  DB: D1Database;
+};
+
+type Params = {
+  id: string;
+};
+
+export class CustomerWorkflow extends WorkflowEntrypoint<Env, Params> {
+  async run(event: WorkflowEvent<Params>, step: WorkflowStep) {
+    const { DB } = this.env;
+
+    const customer = await step.do('fetch customer', async () => {
+      const resp = await DB.prepare(`SELECT * FROM customers WHERE id = ?`)
+        .bind(event.payload.id)
+        .run();
+      if (resp.success) return resp.results[0];
+      return null;
+    });
+
+    if (customer) {
+      await step.do('conditional customer step', async () => {
+        console.log('Hello from a conditional step!');
+      });
+    }
+
+    await step.do('example step', async () => {
+      console.log('Hello from a step!');
+    });
+  }
+}
+
+export default {
+  async fetch(request, env) {
+    return env.ASSETS.fetch(request);
+  },
+};
